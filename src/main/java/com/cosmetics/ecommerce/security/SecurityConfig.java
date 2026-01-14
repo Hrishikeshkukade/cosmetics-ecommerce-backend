3adbd7789e34bce995b8d30ce5734da3a8438e6f
Hrishikeshkukade <hrishikeshkukade6@gmail.com>
2026-01-14 21:39:29 +0530

    application.yml changed

--- src/main/java/com/cosmetics/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/cosmetics/ecommerce/security/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://cosmetics-ecommerce-frontend.vercel.app/", "https://*.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
